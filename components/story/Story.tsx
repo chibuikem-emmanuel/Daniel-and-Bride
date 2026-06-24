b62a08cdd4c80cc8428bc4c37d18703ccaea7bb4
Chibuikem Emmanuel <152220208+chibuikem-emmanuel@users.noreply.github.com>
2026-06-24 21:35:11 +0100

    Update names and location in Story component

--- components/story/Story.tsx
+++ components/story/Story.tsx
@@ -94,13 +94,13 @@ export default function Story() {
           <div className="pt-6 border-t border-[#8DD2DB]/20 flex items-center justify-between">
             <div>
               <p className="text-3xl font-serif text-[#FFCC47]">
-                Joshua
+                Daniel
                 <span className="text-base text-[#8DD2DB] mx-1"> & </span>
-                Divine
+                Rejoice
               </p>
 
               <p className="text-xs uppercase tracking-widest font-black text-[#8DD2DB]/60 mt-1">
-                Lagos, Nigeria
+                Aba, Nigeria
               </p>
             </div>
 
